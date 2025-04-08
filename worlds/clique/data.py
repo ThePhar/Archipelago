c4a38f682df4095ff89b4ce92ca731e44f9eb178
Zach Parks <zach@alliware.com>
2025-04-08 15:41:51 -0500

    use a smaller `BASE_ID`

--- worlds/clique/data.py
+++ worlds/clique/data.py
@@ -1,5 +1,5 @@
 # I realize I don't need a base ID anymore (I wrote PR #1933, okay?), I just like having '69' in the id, huehuehue. >:3
-BASE_ID = 69_000_000
+BASE_ID = 69_000
 
 POSSIBLE_BUTTON_COLORS = [
     "red",
